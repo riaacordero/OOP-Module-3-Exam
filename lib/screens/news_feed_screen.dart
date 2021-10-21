d8a4613ef60cd039391f6567985cc160a575f282
hrjugar <hernanjugar123@gmail.com>
2021-10-22 03:43:06 +0800

    Changed news_feed name to news_feed_screen

---
+++ lib/screens/news_feed_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class NewsFeedScreen extends StatefulWidget {
+  const NewsFeedScreen({ Key? key }) : super(key: key);
+
+  @override
+  _NewsFeedScreenState createState() => _NewsFeedScreenState();
+}
+
+class _NewsFeedScreenState extends State<NewsFeedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Feed")
+      )
+      
+    );
+  }
+}
