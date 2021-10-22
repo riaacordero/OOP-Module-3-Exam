dff54e211936a049e2c8f458905a9ae3b021c78a
hrjugar <hernanjugar123@gmail.com>
2021-10-22 08:19:36 +0800

    Updated new post screen

--- lib/screens/new_post_screen.dart
+++ lib/screens/new_post_screen.dart
@@ -11,54 +11,80 @@ class CreatePostScreen extends StatefulWidget {
 
 class _CreatePostScreenState extends State<CreatePostScreen> {
   String _postMessage = "";
-  bool _loading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: const Color(0xFF0056D7),
-          centerTitle: true,
-          title: const Text("New Post")),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            const Padding(padding: EdgeInsets.symmetric(vertical: 5)),
-            TextField(
-              maxLength: 240,
-              maxLines: 7,
-              decoration: const InputDecoration(
-                hintText: "Message",
-              ),
-              onChanged: (value) {
-                _postMessage = value;
-              },
-            ),
-            const Padding(padding: EdgeInsets.symmetric(vertical: 5)),
-            ElevatedButton(
-              onPressed: () async {
-                setState(() {
-                  _loading = true;
-                });
-                if (_postMessage != null && _postMessage.isNotEmpty) {
-                  // Post newPost = Post(user: currentUser, _postMessage);
-                  Navigator.pop(context);
-                  setState(() {
-                    _loading = false;
-                  });  
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Please type something!')));
-                }
-              },
-              child: const Text("Publish Post"),
-              style: ElevatedButton.styleFrom(
-                  primary: const Color(0xFF0056D7),
-                  shape: const StadiumBorder()),
-            )
-          ],
+        leading: const CloseButton(),
+        backgroundColor: const Color(0xFF0056D7),
+        centerTitle: true,
+        elevation: 0,
+        title: const Text("New Post", 
+          style: TextStyle(fontWeight: FontWeight.bold)
         ),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              if (_postMessage.isNotEmpty) {
+                setState(() {
+                  posts.add(
+                    Post(
+                      user: currentUser, 
+                      message: _postMessage, 
+                      timePosted: DateTime.now()
+                    )
+                  );                  
+                });
+                Navigator.pop(context);
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Please type something!')));
+              }
+            },
+            icon: const Icon(Icons.chevron_right, size: 30)
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: SizedBox(
+          height: double.infinity,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: 50,
+                height: 50,
+                child: CircleAvatar(
+                  backgroundImage: currentUser.profilePicture.image,
+                ),
+              ),
+              const SizedBox(width: 20),
+              Expanded(
+                child: TextField(
+                  autofocus: true,
+                  maxLength: 240,
+                  maxLines: 999999,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Message",
+                    counterText: ""
+                  ),
+                  style: const TextStyle(
+                    fontSize: 20
+                  ),
+                  onChanged: (value) {
+                    setState(() => _postMessage = value);
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: CircularProgressIndicator(
+        value: _postMessage.length / 240,
       ),
     );
   }
